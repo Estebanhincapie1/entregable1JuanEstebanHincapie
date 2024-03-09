527b287a68cd7b56bf0c16084e8ecaf1898c68e9
Estebanhincapie1 <esteban.hincapie1@udea.edu.co>
2024-03-09 15:17:22 -0500

    Agregue unas funciones para que el codigo no me lance error

--- funcionesExternas.py
+++ funcionesExternas.py
@@ -9,3 +9,45 @@ def obtener_nombre():
         # Convertir el nombre a minúsculas
         nombre = nombre.lower()
         return nombre
+
+
+from datetime import datetime
+
+def validar_fecha(fecha_str):
+    try:
+        # Intenta convertir la cadena en un objeto de fecha
+        fecha = datetime.strptime(fecha_str, '%d/%m/%Y')
+        return True, fecha
+    except ValueError:
+        # Si hay un error en la conversión, la fecha no es válida
+        return False, None
+
+
+def pedir_fecha():
+    fecha_valida = False
+    while not fecha_valida:
+        fecha_str = input("Por favor, introduce una fecha en formato dd/mm/aaaa: ")
+        fecha_valida, fecha = validar_fecha(fecha_str)
+        if not fecha_valida:
+            print("Fecha inválida. Por favor, inténtalo de nuevo.")
+    return fecha
+
+
+def datoEntero(mns):
+    while True:
+        dato = input(mns)
+        if dato.isnumeric() == True:
+            return int(dato)
+        else:
+            print('DATO ERRONEO, POR FAVOR INTENTE DE NUEVO')
+            continue
+
+def datoFloat(mns):
+    while True:
+        try:
+            dato = float(input(mns))
+            return dato
+        except ValueError:
+            print('dato erroneo')
+            continue
+        
